c9bed27090f6793fac711d57c3a54d9baa1f03d3
h <hugo.duda@protonmail.com>
2026-03-19 13:08:05 +0100

    < :building_construction: >[Main/Client] Change client vertices

--- examples/test/src/main.rs
+++ examples/test/src/main.rs
@@ -5,9 +5,9 @@ fn main()
     let mut win: Window = Window::new(1920, 1080, "Window")
     .expect("Failed");
     let vertex: Graphics = Graphics::create_vertices(
-        VMNLVertex { position: [1920.0, 1080.0], color: [0.0, 0.0, 255.0] },
-        VMNLVertex { position: [0.0,  1080.0], color: [0.0, 0.0, 100.0] },
-        VMNLVertex { position: [1920.0, 0.0], color: [0.0, 0.0, 100.0] }
+        VMNLVertex { position: [1020.0, 800.0], color: [255.0, 0.0, 0.0] },
+        VMNLVertex { position: [400.0,    800.0], color: [255.0, 0.0, 0.0] },
+        VMNLVertex { position: [1020.0, 400.0], color: [255.0, 0.0, 0.0] }
     );
 
     while win.is_open() {
