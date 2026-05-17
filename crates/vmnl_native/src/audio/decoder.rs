409ef4662ea01c333335bead883379f51ba411f9
nate <nathan.flachat@epitech.eu>
2026-05-17 21:20:46 +0200

    <🏗️  & 📝>[feature/miniaudio_wrapper] add initial audio architecture and implement core audio abstraction

--- crates/vmnl_native/src/audio/decoder.rs
+++ crates/vmnl_native/src/audio/decoder.rs
@@ -3,3 +3,41 @@
 /// SPDX-License-Identifier: MIT
 ///
 ////////////////////////////////////////////////////////////////////////////////
+
+use crate::audio::error::AudioError;
+
+use std::path::Path;
+
+#[derive(Debug, Clone)]
+pub struct DecodedAudio
+{
+    pub channels: u32,
+    pub sample_rate: u32,
+    pub samples: Vec<f32>
+}
+
+pub struct AudioDecoder;
+
+impl AudioDecoder
+{
+    pub fn decode_file<P>(path: P) -> Result<DecodedAudio, AudioError>
+    where
+        P: AsRef<Path>
+    {
+        let path = path.as_ref();
+
+        if !path.exists()
+        {
+            return Err(AudioError::DecoderFailed(format!("File does not exist: {:}",path)));
+        }
+
+        // TODO:
+        // Miniaudio decoder integration
+
+        Ok(DecodedAudio {
+            channels: 2,
+            sample_rate: 44100,
+            samples: Vec::new(),
+        })
+    }
+}
